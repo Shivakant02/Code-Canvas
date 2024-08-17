3b2db3487bece0d89e91379063fe886b071be282
Shivakant <kants6397@gmail.com>
2024-08-17 11:37:23 +0530

    homepage added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,12 @@
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className=" w-full h-[calc(100vh-60px)] text-white flex justify-center items-center flex-col gap-3">
+      <h1 className=" text-center text-6xl font-bold ">CodeCanvas</h1>
+      <p className=" text-center text-gray-400">
+        Compile HTML, CSS, JavaScript code on the go and share it with friends
+      </p>
+    </div>
+  );
 }
 
 export default Home;
