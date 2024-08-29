f8e4b625ed8bc68198279628d7b0a57a53ee0c83
Shivakant <kants6397@gmail.com>
2024-08-29 16:31:07 +0530

    login,signup routes added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import Compile from "./pages/Compile";
 import NotFound from "./pages/NotFound";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/compile" element={<Compile />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/compile/:urlId" element={<Compile />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
