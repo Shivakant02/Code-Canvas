4584ee687000a2508c4cdb0fda9b2dca1e5b3c80
Shivakant <kants6397@gmail.com>
2024-08-30 22:47:57 +0530

    rtk query setup

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import compilerReducer from "./slices/compilerSlice";
+import { authApi } from "./slices/authApi";
 const store = configureStore({
   reducer: {
+    [authApi.reducerPath]: authApi.reducer,
     compiler: compilerReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(authApi.middleware),
 });
 
 export default store;
