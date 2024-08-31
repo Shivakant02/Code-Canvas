58757c6ff5d847e3d44dc6265e37f8c34a1f5836
Shivakant <kants6397@gmail.com>
2024-08-31 21:58:48 +0530

    cors setup

--- server/src/index.js
+++ server/src/index.js
@@ -8,7 +8,12 @@ config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: [process.env.CLIENT_URL],
+    credentials: true,
+  })
+);
 
 app.use("/compiler", compilerRoutes);
 app.use("/user", userRoutes);
