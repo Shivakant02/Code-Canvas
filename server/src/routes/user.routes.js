9a1dfd910c1701374250ad05c4d243fabfc29106
Shivakant <kants6397@gmail.com>
2024-09-02 23:00:47 +0530

    get user details route added

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
-import { login, logout, signup } from "../controllers/user.controller.js";
+import {
+  getUserData,
+  login,
+  logout,
+  signup,
+} from "../controllers/user.controller.js";
+import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+router.get("/user-details", verifyToken, getUserData);
 export default router;
