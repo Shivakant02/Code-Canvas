405ac28a7e375e0b3bab8c050a1455fbacfaee72
Shivakant <kants6397@gmail.com>
2024-09-08 15:00:02 +0530

    save code if user is logged in

--- server/src/routes/copmiler.routes.js
+++ server/src/routes/copmiler.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { loadCode, saveCode } from "../controllers/compiler.controller.js";
+import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = Router();
 
-router.post("/save", saveCode);
+router.post("/save", verifyToken, saveCode);
 router.post("/load", loadCode);
 
 export default router;
