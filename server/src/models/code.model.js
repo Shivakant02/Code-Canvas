7b7925c23bd3b28e0e2e7cc06c4a0403e4479495
Shivakant <kants6397@gmail.com>
2024-09-08 22:12:04 +0530

    typo fix

--- server/src/models/code.model.js
+++ server/src/models/code.model.js
@@ -1,15 +1,18 @@
 import { model, Schema } from "mongoose";
 
-const codeSchema = Schema({
-  fullCode: {
-    html: String,
-    css: String,
-    javascript: String,
+const codeSchema = Schema(
+  {
+    fullCode: {
+      html: String,
+      css: String,
+      javascript: String,
+    },
+    title: { type: String, required: true },
+    ownerInfo: { type: Schema.Types.ObjectId, ref: "User" },
+    ownerName: String,
   },
-  title: { type: String, required: true },
-  ownerInfo: { type: Schema.Types.ObjectId, ref: "User" },
-  ownerName: String,
-});
+  { timestamps: true }
+);
 
 const Code = model("code", codeSchema);
 export default Code;
