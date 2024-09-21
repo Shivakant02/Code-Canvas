2acc242b00dcd85b4e42a172482792315f646893
Shivakant <kants6397@gmail.com>
2024-09-21 17:32:31 +0530

    header config

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -2,10 +2,10 @@ import User from "../models/user.model.js";
 import AppError from "../utils/AppError.js";
 
 const cookieOptions = {
-  expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
-  httpOnly: true,
-  sameSite: "lax",
-  path: "/",
+  expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+  sameSite: "none",
+  secure: true,
+  httpOnly: false,
 };
 
 export const signup = async (req, res, next) => {
