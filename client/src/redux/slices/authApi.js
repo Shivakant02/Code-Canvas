68614b62f81878c7fe33bdc13be6a5f6082d3087
Shivakant <kants6397@gmail.com>
2024-09-21 16:27:59 +0530

    .env  configured

--- client/src/redux/slices/authApi.js
+++ client/src/redux/slices/authApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const authApi = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_APP_API_URL,
+    baseUrl: import.meta.env.VITE_APP_API_URI,
     credentials: "include",
   }),
   tagTypes: ["myCodes"],
